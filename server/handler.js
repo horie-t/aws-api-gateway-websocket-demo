b58c78713b484eb72973f62698401130337b7b36
HORIE Tetsuya <horie-t@users.noreply.github.com>
2021-06-19 21:46:58 +0900

    サーバ側を実装

--- server/handler.js
+++ server/handler.js
@@ -1,14 +1,83 @@
-'use strict';
+const AWS = require('aws-sdk');
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!'
-      },
-      null,
-      2
-    ),
+const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
+
+const { TABLE_NAME } = process.env;
+
+async function onConnect(event) {
+  const putParams = {
+    TableName: TABLE_NAME,
+    Item: {
+      connectionId: event.requestContext.connectionId
+    }
   };
+
+  try {
+    await ddb.put(putParams).promise();
+  } catch (err) {
+    return { statusCode: 500, body: 'Failed to connect: ' + JSON.stringify(err) };
+  }
+
+  return { statusCode: 200, body: 'Connected.' };
 };
+
+async function onDiscconect(event) {
+  const deleteParams = {
+    TableName: TABLE_NAME,
+    Key: {
+      connectionId: event.requestContext.connectionId
+    }
+  };
+
+  try {
+    await ddb.delete(deleteParams).promise();
+  } catch (err) {
+    return { statusCode: 500, body: 'Failed to disconnect: ' + JSON.stringify(err) };
+  }
+
+  return { statusCode: 200, body: 'Disconnected.' };
+};
+
+async function broadcast(event) {
+  let connectionData;
+  
+  try {
+    connectionData = await ddb.scan({ TableName: TABLE_NAME, ProjectionExpression: 'connectionId' }).promise();
+  } catch (e) {
+    return { statusCode: 500, body: e.stack };
+  }
+  
+  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+    apiVersion: '2018-11-29',
+    endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
+  });
+  
+  const postData = JSON.parse(event.body).data;
+  
+  const postCalls = connectionData.Items.map(async ({ connectionId }) => {
+    try {
+      await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
+    } catch (e) {
+      if (e.statusCode === 410) {
+        console.log(`Found stale connection, deleting ${connectionId}`);
+        await ddb.delete({ TableName: TABLE_NAME, Key: { connectionId } }).promise();
+      } else {
+        throw e;
+      }
+    }
+  });
+  
+  try {
+    await Promise.all(postCalls);
+  } catch (e) {
+    return { statusCode: 500, body: e.stack };
+  }
+
+  return { statusCode: 200, body: 'Data sent.' };
+};
+
+module.exports = {
+  onConnect,
+  onDiscconect,
+  broadcast
+}
